df8709038bbebfbeb5d34dc892145572ca625d6a
dario-nihil <dario.nihil@gmail.com>
2022-02-15 21:22:31 +0100

    created events components, added styles and svg icon and also created button component and relative style

---
+++ components/events/event-list.jsx
@@ -0,0 +1,15 @@
+import EventItem from "./event-item";
+
+import styles from "./event-list.module.css";
+
+const EventList = ({ items }) => {
+  return (
+    <ul className={styles.list}>
+      {items.map((item) => (
+        <EventItem key={item.id} {...item} />
+      ))}
+    </ul>
+  );
+};
+
+export default EventList;
